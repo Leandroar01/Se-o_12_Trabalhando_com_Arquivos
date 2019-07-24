ee48beed5c265f66fc1a10cf536251b0bda681e2
Leandro Rodrigues <leandroar01@hotmail.com>
2019-07-24 20:10:49 -0300

    start

---
+++ Aula163_ExercicioProposto.java
@@ -0,0 +1,19 @@
+package Secao_12_Trabalhando_com_Arquivos;
+
+public class Aula163_ExercicioProposto {
+
+	/**
+	 * Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos. 
+	 * Cada item possui um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar 
+	 * um novo arquivo 	 * chamado "summary.csv", localizado em uma subpasta chamada "out" a partir 
+	 * da pasta original do arquivo deorigem, contendoapenas onomeeovalortotal para aquele 
+	 * item (preço unitário multiplicado pela quantidade
+	 */
+	
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
